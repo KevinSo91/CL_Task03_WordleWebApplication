7b0fb545f8553f818d50ceefc04dc9f37c6a2c71
Kevin S <Kevin.Sommer91@freenet.de>
2022-04-06 11:00:25 +0200

    Filter Anzeige hinzugefügt

--- src/main/java/mainPackage/controller/DictionaryController.java
+++ src/main/java/mainPackage/controller/DictionaryController.java
@@ -22,18 +22,41 @@ public class DictionaryController {
 
 	@GetMapping()
 	public String getDictionary(Model model, @RequestParam(required = false) String languageFilter, @RequestParam(required = false) String wordLengthFilter) {
-		if (languageFilter != "all" && wordLengthFilter != "0") {
-			model.addAttribute("woerterListe", wortService.findeAlleWoerterAusSpracheMitWortlaenge(languageFilter, Integer.parseInt(wordLengthFilter)));
+		
+		if(languageFilter == null) {
+			languageFilter = "all";
 		}
-		else if(languageFilter != "all" && wordLengthFilter == "0") {
-			model.addAttribute("woerterListe", wortService.findeAlleWoerterAusSprache(languageFilter));
+		
+		if(wordLengthFilter == null) {
+			wordLengthFilter = "0";
 		}
-		else if(languageFilter == "all" && wordLengthFilter != "0") {			
-			model.addAttribute("woerterListe", wortService.findeAlleWoerterMitWortLaenge(Integer.parseInt(wordLengthFilter)));			
-		}
-		else {			
+		
+		// Keine Filter(RequestParams) angegeben oder Filter: Alles anzeigen
+		if((languageFilter == "all" && wordLengthFilter == "0") || (languageFilter == null && wordLengthFilter == null)) {
 			model.addAttribute("woerterListe", wortService.findeAlleWoerter());
+		}
+		
+		else {			
+			// Nur Filter für Sprache ausgewählt
+			if(languageFilter != "all" && wordLengthFilter == "0") {
+				model.addAttribute("woerterListe", wortService.findeAlleWoerterAusSprache(languageFilter));
+			}
+			// Nur Filter für Wortlänge ausgewählt
+			else if(languageFilter == "all" && wordLengthFilter != "0") {			
+				model.addAttribute("woerterListe", wortService.findeAlleWoerterMitWortLaenge(Integer.parseInt(wordLengthFilter)));			
+			}
+			// Filter für Sprache und Wortlänge ausgewählt
+			else {			
+				model.addAttribute("woerterListe", wortService.findeAlleWoerterAusSpracheMitWortlaenge(languageFilter, Integer.parseInt(wordLengthFilter)));
+			}			
 		}		
+		
+		// Anzeige der aktiven Filter
+		String[] filterParams = {languageFilter, wordLengthFilter};
+		if(filterParams[1] == "0") {
+			filterParams[1] = "all";
+		}
+		model.addAttribute("filter", filterParams);
 		
 		return "dictionary";
 	}
--- src/main/java/mainPackage/controller/SettingsController.java
+++ src/main/java/mainPackage/controller/SettingsController.java
@@ -11,19 +11,11 @@ import mainPackage.wort.WortService;
 @Controller
 @RequestMapping("/settings")
 public class SettingsController {
-
-	private final WortService wortService;
 	
 	@Autowired
 	public SettingsController(WortService wortService) {
-		this.wortService = wortService;
 	}
 	
-//	@GetMapping("/dictionary")
-//	public String getDictionary(Model model) {
-//		model.addAttribute("woerterListe", wortService.findeAlleWoerter());
-//		return "dictionary";
-//	}
 	
 	@GetMapping("/manual")
 	public String getManual() {
