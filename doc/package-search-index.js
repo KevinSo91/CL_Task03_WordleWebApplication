7794814f3df8605bcb39b4ad7b1fa8628bd62c07
Kevin S <Kevin.Sommer91@freenet.de>
2022-08-31 16:33:47 +0200

    Stand 31.08.

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"mainPackage"},{"l":"mainPackage.controller"},{"l":"mainPackage.game"},{"l":"mainPackage.wort"}];updateSearchResults();
